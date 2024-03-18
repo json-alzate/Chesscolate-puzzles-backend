da60a133cdd29d09280671c1b8d3fd557b04646c
json-alzate <developer.alzate@gmail.com>
2024-03-18 11:34:10 -0500

    unzip from /puzzlesdata/storage

--- src/puzzles/load.service.ts
+++ src/puzzles/load.service.ts
@@ -21,34 +21,34 @@ export class LoadService implements OnModuleInit {
     private puzzleOpeningsCache = new Map<string, any[]>(); // Añade caché para puzzles
 
     async onModuleInit() {
-        // // Ruta del archivo .zip
-        // const zipFilePath = join('/puzzlesdata/puzzles_folder.zip');
-        // // Directorio donde descomprimir
-        // const extractPath = join('/puzzlesdata');
-        // console.log('__dirname v2 fix', zipFilePath, extractPath);
-        // createReadStream(zipFilePath)
-        //     .pipe(unzipper.Extract({ path: extractPath }))
-        //     .on('finish', () => console.log('Descompresión completada.'))
-        //     .on('error', (err) => console.error('Error al descomprimir:', err));
+        // Ruta del archivo .zip
+        const zipFilePath = join('/puzzlesdata/storage/puzzles_folder.zip');
+        // Directorio donde descomprimir
+        const extractPath = join('/puzzlesdata/storage/');
+        console.log('__dirname v2 fix', zipFilePath, extractPath);
+        createReadStream(zipFilePath)
+            .pipe(unzipper.Extract({ path: extractPath }))
+            .on('finish', () => console.log('Descompresión completada.'))
+            .on('error', (err) => console.error('Error al descomprimir:', err));
 
 
-        const dirPath = '/puzzlesdata';
-        readdir(dirPath, (err, files) => {
-            if (err) {
-                console.error(`Error al listar los archivos en: ${dirPath}`, err);
-                return;
-            }
-            console.log(`Archivos en ${dirPath}:`, files);
-        });
+        // const dirPath = '/puzzlesdata';
+        // readdir(dirPath, (err, files) => {
+        //     if (err) {
+        //         console.error(`Error al listar los archivos en: ${dirPath}`, err);
+        //         return;
+        //     }
+        //     console.log(`Archivos en ${dirPath}:`, files);
+        // });
 
-        const dirPath2 = '/puzzlesdata/storage';
-        readdir(dirPath2, (err, files) => {
-            if (err) {
-                console.error(`Error al listar los archivos en: ${dirPath2}`, err);
-                return;
-            }
-            console.log(`Archivos en ${dirPath2}:`, files);
-        });
+        // const dirPath2 = '/puzzlesdata/storage';
+        // readdir(dirPath2, (err, files) => {
+        //     if (err) {
+        //         console.error(`Error al listar los archivos en: ${dirPath2}`, err);
+        //         return;
+        //     }
+        //     console.log(`Archivos en ${dirPath2}:`, files);
+        // });
         // // await this.loadPuzzlesThemesIndex();
         // await this.loadPuzzlesOpeningIndex();
     }
